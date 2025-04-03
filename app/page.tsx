fce7cf53f79e121f3abb1ff52526dade1390adb2
Jarren San Jose <sanjose.jarren@gmail.com>
2025-04-02 23:48:00 -0700

    edit page layout to display things better

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import SkillTreeSelector from "@/components/SkillTreeSelector";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-[32px] items-center w-full">
         <h1 className="text-4xl font-bold text-center">skill tree</h1>
         <SkillTreeSelector />
       </main>
